47865de004505140126eccc8c97bb3967a9b1f0e
gugaasrikanth <gugaa.srikanth@alumni.ipl.university>
2025-03-30 17:13:06 +0530

    Integrating with Clerk for signin/signup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -12,8 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
-      <body className="bg-white min-h-screen">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" data-theme="light">
+        <body className="bg-white min-h-screen">
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
